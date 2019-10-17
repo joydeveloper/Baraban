d26d20ccf910f6f367752fd4d8166ef5a3ea7ab5
joydeveloper <joy2431@list.ru>
2019-10-18 02:52:18 +0700

    core_ver 0.01

---
+++ barabanjs/core/startup.js
@@ -0,0 +1,14 @@
+
+let appconfig = {
+    title: 'BarabanJS',
+    screenMode: 'auto',
+    linux: 'linux.css',
+    windows: 'windows.css',
+    mac: 'mac.css',
+    android: 'android.css',
+    iphone: 'iphone.css',
+    ipad: 'ipad.css',
+    default: 'default.css'
+};
+var app = new AppX(appconfig);
+app.Setup();
--- barabanjs/utils/utils.js
+++ barabanjs/utils/utils.js
@@ -7,8 +7,6 @@ function loadCss(href) {
 function loadScript(src, callback) {
     let script = document.createElement('script');
     script.src = src;
-  
     script.onload = () => callback(script);
-  
     document.head.append(script);
-  }
+}
